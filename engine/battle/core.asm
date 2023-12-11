a5292f85bb676fe85979463c02f757172ef25ebd
Craig Livingstone <2470470l@student.gla.ac.uk>
2023-12-11 20:08:33 +0000

    Fixed Dual-type move effectiveness may be misreported bug - https://github.com/pret/pokered/wiki/%5BARCHIVED%5D-Bugs-and-Glitches#dual-type-move-effectiveness-may-be-misreported

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5443,6 +5443,21 @@ AdjustDamageForMoveType:
 	and $80
 	ld b, a
 	ld a, [hl] ; a = damage multiplier
+	and a  ; cp NO_EFFECT
+	jr z, .gotMultiplier
+	cp NOT_VERY_EFFECTIVE
+	jr nz, .nothalf
+	ld a, [wDamageMultipliers]
+	and $7f
+	srl a
+	jr .gotMultiplier
+.nothalf
+	cp SUPER_EFFECTIVE
+	jr nz, .gotMultiplier
+	ld a, [wDamageMultipliers]
+	and $7f
+	sla a
+.gotMultiplier
 	ldh [hMultiplier], a
 	add b
 	ld [wDamageMultipliers], a
