20da3a1cb01a283efa37e21ada600b54efdb7284
thesmithplays <patricksmith183@gmail.com>
2024-01-04 13:23:27 -0400

    vulpix and poliwag to 15%

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -3,10 +3,10 @@ Route2WildMons:
 	db  3, RATTATA
 	db  3, PIDGEY
 	db  4, RATTATA
-	db  4, VULPIX
+	db  4, NIDORAN_M
 	db  5, VULPIX
 	db  5, PIDGEY
-	db  6, NIDORAN_M
+	db  5, VULPIX
 	db  6, NIDORAN_F
 	db  7, PIDGEY
 	db  7, PIDGEY
--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,15 +1,15 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  2, NIDORAN_M
+	db  3, NIDORAN_M
 	db  4, MANKEY
 	db  3, NIDORAN_F
-	db  3, POLIWAG
-	db  4, POLIWAG
 	db  4, NIDORAN_M
+	db  3, POLIWAG
+	db  4, NIDORAN_F
 	db  4, SPEAROW
 	db  6, SPEAROW
-	db  4, NIDORAN_F
-	db  6, NIDORAN_F
+	db  4, POLIWAG
+	db  5, POLIWAG
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
