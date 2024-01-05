8b785056ee3bc95549175c9b0179f04a333a0a57
thesmithplays <patricksmith183@gmail.com>
2024-01-05 14:54:43 -0400

    cocoons get damage moves

--- data/pokemon/base_stats/metapod.asm
+++ data/pokemon/base_stats/metapod.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/metapod.pic", 0, 1 ; sprite dimensions
 	dw MetapodPicFront, MetapodPicBack
 
-	db HARDEN, NO_MOVE, NO_MOVE, NO_MOVE ; level 1 learnset
+	db TACKLE, HARDEN, STRING_SHOT, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
