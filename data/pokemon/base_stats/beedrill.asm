2dedad18a2240416686c7064cf210ed2b8dad8aa
thesmithplays <patricksmith183@gmail.com>
2024-02-05 09:48:49 -0400

    butterfree and beedrill level 1 learnsets

--- data/pokemon/base_stats/beedrill.asm
+++ data/pokemon/base_stats/beedrill.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/beedrill.pic", 0, 1 ; sprite dimensions
 	dw BeedrillPicFront, BeedrillPicBack
 
-	db FURY_ATTACK, NO_MOVE, NO_MOVE, NO_MOVE ; level 1 learnset
+	db STRING_SHOT, HARDEN, POISON_STING, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
