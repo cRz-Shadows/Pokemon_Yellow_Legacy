5b3c0283fe172bb4cc3fee67fcf8297e3e14faf6
thesmithplays <patricksmith183@gmail.com>
2024-02-10 09:25:56 -0400

    fire type special buff & added pokemon to mid game routes for grass and fire types

--- data/pokemon/base_stats/charizard.asm
+++ data/pokemon/base_stats/charizard.asm
@@ -1,6 +1,6 @@
 	db DEX_CHARIZARD ; pokedex id
 
-	db  78,  84,  78, 100,  85
+	db  78,  84,  78, 100,  95
 	;   hp  atk  def  spd  spc
 
 	db FIRE, FLYING ; type
--- data/pokemon/base_stats/charmeleon.asm
+++ data/pokemon/base_stats/charmeleon.asm
@@ -1,6 +1,6 @@
 	db DEX_CHARMELEON ; pokedex id
 
-	db  58,  64,  58,  80,  65
+	db  58,  64,  58,  80,  70
 	;   hp  atk  def  spd  spc
 
 	db FIRE, FIRE ; type
