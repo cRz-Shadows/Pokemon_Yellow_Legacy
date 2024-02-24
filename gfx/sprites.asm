aa8c878fd85e5bff09d2cc4c0213e834d4c1cc0a
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-02-24 15:23:20 +0000

    Mewtwo now has a unique sprite

--- gfx/sprites.asm
+++ gfx/sprites.asm
@@ -89,6 +89,7 @@ SurfingPikachuSprite::   INCBIN "gfx/sprites/surfing_pikachu.2bpp"
 JessieSprite::           INCBIN "gfx/sprites/jessie.2bpp"
 JamesSprite::            INCBIN "gfx/sprites/james.2bpp"
 BaldingGuySprite::       INCBIN "gfx/sprites/balding_guy.2bpp"
+MewtwoSprite::         INCBIN "gfx/sprites/mewtwo.2bpp"
 KrisSprite::             INCBIN "gfx/sprites/kris.2bpp"
 
 
