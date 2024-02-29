a606db66fbf695d71335a1f9bfeff6dd9628fc37
thesmithplays <patricksmith183@gmail.com>
2024-02-29 10:12:24 -0400

    eevee with just tackle

--- data/pokemon/base_stats/eevee.asm
+++ data/pokemon/base_stats/eevee.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/eevee.pic", 0, 1 ; sprite dimensions
 	dw EeveePicFront, EeveePicBack
 
-	db TACKLE, TAIL_WHIP, NO_MOVE, NO_MOVE ; level 1 learnset
+	db TACKLE, NO_MOVE, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
