e3071222941838e7e916d777fa25a95687acd6e5
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-03-02 21:14:35 +0000

    Venusaur gets rock slide

--- data/pokemon/base_stats/venusaur.asm
+++ data/pokemon/base_stats/venusaur.asm
@@ -17,7 +17,7 @@
 	tmhm SWORDS_DANCE, TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  \
 	     HYPER_BEAM,   RAGE,         MEGA_DRAIN,   SOLARBEAM,    MIMIC,        \
 	     DOUBLE_TEAM,  REFLECT,      BIDE,         REST,         SUBSTITUTE,   \
-	     CUT
+	     CUT,		   ROCK_SLIDE
 	; end
 
 	db 0 ; padding
