9bb2eb2806805d14e866c7c77049067cb5376d85
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-03-29 00:49:28 +0000

    Gave dragonite fly

--- data/pokemon/base_stats/dragonite.asm
+++ data/pokemon/base_stats/dragonite.asm
@@ -19,7 +19,7 @@
 	     HYPER_BEAM,   RAGE,         DRAGON_RAGE,  THUNDERBOLT,  THUNDER,      \
 	     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         FIRE_BLAST,   \
 	     SWIFT,        SKULL_BASH,   REST,         THUNDER_WAVE, SUBSTITUTE,   \
-	     SURF,         STRENGTH
+	     SURF,         STRENGTH, 	 FLY
 	; end
 
 	db 0 ; padding
